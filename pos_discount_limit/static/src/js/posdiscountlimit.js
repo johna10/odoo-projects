c0ede01bea2e7e2a3a38b7c94a4b64fbe34c2661
johna10 <alanjohn573@gmail.com>
2025-02-28 18:37:32 +0530

    Get PosStore:

--- pos_discount_limit/static/src/js/posdiscountlimit.js
+++ pos_discount_limit/static/src/js/posdiscountlimit.js
@@ -1,17 +1,17 @@
 import { PosOrderline } from "@point_of_sale/app/models/pos_order_line";
+import { Orderline } from "@point_of_sale/app/generic_components/orderline/orderline";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
+import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { patch } from "@web/core/utils/patch";
 
 console.log('hai')
-
-patch(PosOrderline.prototype, {
-
-    setup(vals) {
-        this.brand_id = this.product_id.brand_id || "";
-        return super.setup(...arguments);
-    },
-
-    getDisplayData() {
+//console.log(this)
+patch(PosStore.prototype, {
+    async pay(){
+    console.log('CLICKED')
     console.log('this ->' , this)
+
+    console.log('this -> discount type ', this.config.discount_type)
     console.log('this -> discount type ', this.config.discount_type)
     console.log('this -> fixed discount limit' , this.config.discount_fixed_limit)
     console.log('this -> percentage discount limit' , this.config.discount_percentage_limit)
@@ -19,31 +19,77 @@ patch(PosOrderline.prototype, {
     console.log('this -> global discount')
     console.log('-----------------------------------------------------------------------------')
 
-    if(this.config.discount_type == 'fixed'){
-        console.log('Type is Fixed')
-    }
-    else if(this.config.discount_type == 'percentage'){
-        console.log('Type is Percentage')
-        const limited_discount = this.config.discount_percentage_limit
-        const applied_discount = this.discount
-        if(limited_discount < applied_discount){
-            console.log('Limit')
-              alert('Discount Cross the Limit');
-        }
-        else{
-            console.log('No Limit')
-        }
-    }
-    else{
-        console.log('Nothing')
-    }
+    const currentOrder = this.get_order().get_orderlines();
+    console.log('Current Orders', currentOrder)
+    console.log('Current Orders length', currentOrder.length)
 
-        return {
-            ...super.getDisplayData(),
-            brand: this.brand_id.name || "",
-        };
-    },
+    const Total_discount_limit_balance = this.config.discount_fixed_limit
+
+    for (var i = 0; i < currentOrder.length; i++) {
+
+         const discount =  this.discount
+         const discounted_amount = currentOrder[i].price_subtotal
+         const final_price = currentOrder[i].price_subtotal * currentOrder[i].qty
+         actual_price = currentOrder[i].price_unit * currentOrder[i].qty
+         discount_taken = actual_price - final_price
+
+         console.log(currentOrder[i].full_product_name)
+         console.log('Discount', discount)
+         console.log('Discounted Amount', discounted_amount)
+         console.log('final Price', final_price)
+         console.log('Actual Price', actual_price )
+         console.log('Discount taken', discount_taken )
+
+
+        }
+    }
 });
+
+
+//patch(PosOrderline.prototype, {
+//
+//    setup(vals) {
+//        this.brand_id = this.product_id.brand_id || "";
+//        return super.setup(...arguments);
+//    },
+//
+//    getDisplayData() {
+//    console.log('this ->' , this)
+//
+//    if(this.config.discount_type == 'fixed'){
+//        console.log('Type is Fixed')
+//        const per_session_limit_amount = this.config.discount_fixed_limit
+//        console.log('Fixed Amount', per_session_limit_amount - 1)
+//        console.log('Unit price', this.price_unit)
+//        const currentOrder = this.get_order();
+//        console.log('Current Orders', currentOrder)
+//
+//
+//
+//
+//    }
+//    else if(this.config.discount_type == 'percentage'){
+//        console.log('Type is Percentage')
+//        const limited_discount = this.config.discount_percentage_limit
+//        const applied_discount = this.discount
+//        if(limited_discount < applied_discount){
+//            console.log('Limit')
+//            alert('Discount Cross the Limit');
+//        }
+//        else{
+//            console.log('No Limit')
+//        }
+//    }
+//    else{
+//        console.log('Nothing')
+//    }
+//
+//        return {
+//            ...super.getDisplayData(),
+//            brand: this.brand_id.name || "",
+//        };
+//    },
+//});
 
 
 
