7593fa5688a29726f4ea934ac65e799b858ea82d
johna10 <alanjohn573@gmail.com>
2025-03-01 17:09:52 +0530

    Functionality added for discount limit in pos

--- pos_discount_limit/static/src/js/posdiscountlimit.js
+++ pos_discount_limit/static/src/js/posdiscountlimit.js
@@ -5,151 +5,46 @@ import { usePos } from "@point_of_sale/app/store/pos_hook";
 import { patch } from "@web/core/utils/patch";
 
 console.log('hai')
-//console.log(this)
 patch(PosStore.prototype, {
     async pay(){
-    console.log('CLICKED')
-    console.log('this ->' , this)
-
-    console.log('this -> discount type ', this.config.discount_type)
-    console.log('this -> discount type ', this.config.discount_type)
-    console.log('this -> fixed discount limit' , this.config.discount_fixed_limit)
-    console.log('this -> percentage discount limit' , this.config.discount_percentage_limit)
-    console.log('this -> line discount' , this.discount)
-    console.log('this -> global discount')
-    console.log('-----------------------------------------------------------------------------')
-
     const currentOrder = this.get_order().get_orderlines();
     console.log('Current Orders', currentOrder)
     console.log('Current Orders length', currentOrder.length)
 
-    const Total_discount_limit_balance = this.config.discount_fixed_limit
+    var Total_discount_limit_balance = this.config.discount_fixed_limit
+    console.log('Limit ', Total_discount_limit_balance);
+    console.log('--------------------------------------------------------------------------------');
 
+    var Dis = 0;
     for (var i = 0; i < currentOrder.length; i++) {
+        const discount = this.discount; // Ensure this is correctly assigned
+        const discounted_amount = currentOrder[i].price_subtotal; // After discount
+        const actual_price = currentOrder[i].price_unit * currentOrder[i].qty; // Original price
+        const final_price = discounted_amount * currentOrder[i].qty; // Total after discount
+        var individual_line_discount = actual_price - final_price; // Discount on this product
 
-         const discount =  this.discount
-         const discounted_amount = currentOrder[i].price_subtotal
-         const final_price = currentOrder[i].price_subtotal * currentOrder[i].qty
-         actual_price = currentOrder[i].price_unit * currentOrder[i].qty
-         discount_taken = actual_price - final_price
+        Dis += individual_line_discount; // Accumulate discount
 
-         console.log(currentOrder[i].full_product_name)
-         console.log('Discount', discount)
-         console.log('Discounted Amount', discounted_amount)
-         console.log('final Price', final_price)
-         console.log('Actual Price', actual_price )
-         console.log('Discount taken', discount_taken )
+        console.log(currentOrder[i].full_product_name);
+        console.log('Discount', discount);
+        console.log('After Discount', discounted_amount);
+        console.log('Final Price', final_price);
+        console.log('Actual Price', actual_price);
+    }
+    console.log('--------------------------------------------------------------------------------');
+    console.log('Total Discount Taken:', Dis);
+    Total_discount_limit_balance = Total_discount_limit_balance - Dis
+    console.log('Discount Balance :', Total_discount_limit_balance);
 
-
-        }
+    if(Total_discount_limit_balance > 0){
+        console.log('Balance present');
+        alert('have Balance ');
+        return super.pay();
+    }
+    else{
+        console.log('No Balance');
+        alert('No Balance');
+    }
     }
 });
 
-
-//patch(PosOrderline.prototype, {
-//
-//    setup(vals) {
-//        this.brand_id = this.product_id.brand_id || "";
-//        return super.setup(...arguments);
-//    },
-//
-//    getDisplayData() {
-//    console.log('this ->' , this)
-//
-//    if(this.config.discount_type == 'fixed'){
-//        console.log('Type is Fixed')
-//        const per_session_limit_amount = this.config.discount_fixed_limit
-//        console.log('Fixed Amount', per_session_limit_amount - 1)
-//        console.log('Unit price', this.price_unit)
-//        const currentOrder = this.get_order();
-//        console.log('Current Orders', currentOrder)
-//
-//
-//
-//
-//    }
-//    else if(this.config.discount_type == 'percentage'){
-//        console.log('Type is Percentage')
-//        const limited_discount = this.config.discount_percentage_limit
-//        const applied_discount = this.discount
-//        if(limited_discount < applied_discount){
-//            console.log('Limit')
-//            alert('Discount Cross the Limit');
-//        }
-//        else{
-//            console.log('No Limit')
-//        }
-//    }
-//    else{
-//        console.log('Nothing')
-//    }
-//
-//        return {
-//            ...super.getDisplayData(),
-//            brand: this.brand_id.name || "",
-//        };
-//    },
-//});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//console.log(pos.config.discount_fixed_limit)
-//console.log(pos.config.discount_percentage_limit)
-
-//import { Component } from "@odoo/owl";
-//import { usePos } from "@point_of_sale/app/store/pos_store";
-//
-//export class MyDiscountComponent extends Component {
-//    setup() {
-//        this.pos = usePos(); // Access POS store
-//    }
-//
-//    get discountLimitEnabled() {
-//        return this.pos.config.is_discount_limit; // Access your custom field
-//    }
-//
-//    get discountType() {
-//        return this.pos.config.discount_type;
-//    }
-//}
-//
-//const pos = odoo.__DEBUG__.services['pos'].pos;
-//
-//function applyDiscount(order, discountValue) {
-//    if (pos.config.is_discount_limit) {
-//        if (pos.config.discount_type === "fixed") {
-//            if (discountValue > pos.config.discount_fixed_limit) {
-//                console.log("Discount exceeds fixed limit!");
-//                return false;
-//            }
-//        } else if (pos.config.discount_type === "percentage") {
-//            if (discountValue > pos.config.discount_percentage_limit) {
-//                console.log("Discount exceeds percentage limit!");
-//                return false;
-//            }
-//        }
-//    }
-//
-//    // Apply the discount if within limit
-//    order.set_discount(discountValue);
-//    return true;
-//}
