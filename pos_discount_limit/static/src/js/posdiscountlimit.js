43f714333c5eb4de0b6c34850c1a1590f1bbb190
johna10 <alanjohn573@gmail.com>
2025-03-04 18:26:45 +0530

    Discount limit-

--- pos_discount_limit/static/src/js/posdiscountlimit.js
+++ pos_discount_limit/static/src/js/posdiscountlimit.js
@@ -19,53 +19,138 @@ patch(PosStore.prototype, {
 
     async pay(){
 
-    console.log('Current Opened Session ID :',this.config.current_session_id.id)
+        console.log('Current Opened Session ID :',this.config.current_session_id.id)
 
-    this.discount_limit_of_session = this.config.discount_fixed_limit;
-    console.log("Limit given to current Shop :", this.discount_limit_of_session)
+        this.discount_limit_of_session = this.config.discount_fixed_limit;
+        console.log("Limit given to current Shop :", this.discount_limit_of_session)
 
-    const currentOrder = this.get_order().get_orderlines();
-    var Dis = 0;
+        const currentOrder = this.get_order().get_orderlines();
 
-    for (var i = 0; i < currentOrder.length; i++) {
-        const discount = this.discount;
-        const discounted_amount = currentOrder[i].price_subtotal; // After discount
-        const actual_price = currentOrder[i].price_unit * currentOrder[i].qty; // Original price
-        const final_price = discounted_amount * currentOrder[i].qty; // Total after discount
-        var individual_line_discount = actual_price - final_price; // Discount on this product
+
+        console.log('Length of the order line ',currentOrder.length )
+
+
+
+
+
+
+
+
+
+        var Dis = 0;
         var global_discount = 0;
-        if(currentOrder[i].full_product_name === "Discount"){
-            global_discount += Math.abs(discounted_amount);
+        var individual_line_discount = 0;
+
+        for (var i = 0; i < currentOrder.length; i++) {
+
+              if(currentOrder[i].full_product_name === "Discount"){
+                    console.log("DISCOUNT PRODUCT")
+                    var discounted_amount = currentOrder[i].price_subtotal;
+                    console.log('Discount product price',discounted_amount)
+                    global_discount += Math.abs(discounted_amount);
+              }
+              else{
+                    console.log(currentOrder[i].discount)
+                    if(currentOrder[i].discount){
+                        console.log('Product have discount ')
+                        var discounted_amount = currentOrder[i].price_subtotal; // After discount
+                        var actual_price = currentOrder[i].price_unit * currentOrder[i].qty; // Original price without discount
+                        var final_price = discounted_amount// Total after discount
+
+                            console.log(' product price after discount applied',discounted_amount)
+                            console.log(' product actual price ',actual_price)
+                            console.log(' product final price',final_price)
+
+                        if(discounted_amount != actual_price){
+                            console.log('Discount is applied')
+                            individual_line_discount += actual_price - final_price; // Discount on this product
+                        }
+                        else{
+                            console.log('Discount is not applied')
+                        }
+                    }
+                    else{
+                            console.log('Product have no discount')
+                    }
+                      console.log(' product individual line discount',individual_line_discount)
+                      console.log('---------------------')
+              }
+
+
+
+
+
+
+
+//            if(currentOrder[i].full_product_name === "Discount"){
+//                console.log("DISCOUNT PRODUCT")
+//                var discounted_amount = currentOrder[i].price_subtotal;
+//                console.log('Discount product price',discounted_amount)
+//                global_discount += Math.abs(discounted_amount);
+//            }
+//            else{
+//                console.log('NON DISCOUNT PRODUCT')
+//                var discounted_amount = currentOrder[i].price_subtotal; // After discount
+//                var actual_price = currentOrder[i].price_unit * currentOrder[i].qty; // Original price without discount
+//                var final_price = discounted_amount// Total after discount
+//
+//                console.log(' product price after discount applied',discounted_amount)
+//                console.log(' product actual price ',actual_price)
+//                console.log(' product final price',final_price)
+//
+//                if(discounted_amount != actual_price){
+//                    console.log('Discount is applied')
+//                    individual_line_discount = actual_price - final_price; // Discount on this product
+//                }
+//                else{
+//                    console.log('Discount is not applied')
+//                }
+//                console.log(' product individual line discount',individual_line_discount)
+//
+//            }
+//            Dis += individual_line_discount + global_discount; // Accumulate discount
+//            console.log('/////////////////////////')
         }
-        Dis += individual_line_discount + global_discount; // Accumulate discount
-    }
 
-    console.log('Discount given for current order:', Dis);
-    let add_to_session_balance = this.Total_discount_limit_balance + Dis
-    console.log('Total Discount given by the session :', add_to_session_balance);
 
-    if( 0 <= add_to_session_balance && add_to_session_balance < this.discount_limit_of_session){
-        console.log('Balance present');
-        this.Total_discount_limit_balance = add_to_session_balance;
-        console.log('Updated Balance from local', this.Total_discount_limit_balance)
 
-       await this.env.services.orm.call(
-                "pos.session",
-                "sample",
-                [
-                this.config.current_session_id.id,
-                this.Total_discount_limit_balance,
-                ]);
 
-        return await super.pay();
-    }
-    else{
-        console.log('No Balance');
-        this.dialog.add(AlertDialog, {
-                title: _t("Discount Limit Exceed."),
-                body: _t('The available balance for discount is Zero'),
-            });
-    }
+
+
+
+
+
+
+
+
+
+
+        console.log('Discount given for current order:', Dis);
+        var add_to_session_balance = this.Total_discount_limit_balance + Dis
+        console.log('Total Discount given by the session :', add_to_session_balance);
+
+        if( 0 <= add_to_session_balance && add_to_session_balance < this.discount_limit_of_session){
+            console.log('Balance present');
+            this.Total_discount_limit_balance = add_to_session_balance;
+            console.log('Updated Balance from local', this.Total_discount_limit_balance)
+
+           await this.env.services.orm.call(
+                    "pos.session",
+                    "sample",
+                    [
+                    this.config.current_session_id.id,
+                    this.Total_discount_limit_balance,
+                    ]);
+
+            return await super.pay();
+        }
+        else{
+            console.log('No Balance');
+            this.dialog.add(AlertDialog, {
+                    title: _t("Discount Limit Exceed."),
+                    body: _t('The available balance for discount is Zero'),
+                });
+        }
     }
 });
 
